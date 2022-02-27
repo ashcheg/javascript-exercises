227478d288d602037d46ded8198e5731d55d29ea
ashcheg <alina.shcheg@gmail.com>
2022-02-26 23:55:32 -0500

    Finished exercises 6 and 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
-
+// from farenheight to celcius
+const ftoc = function(tempFar) {
+  let tempCel = (tempFar - 32) * (5 / 9);
+  if (tempCel == 0) {
+    return 0
+  }
+  return Number(tempCel.toFixed(1));
 };
 
-const ctof = function() {
-
+//from celcius to farenheight
+const ctof = function(tempCel) {
+  let tempFar = tempCel * (9 / 5) + 32;
+  return Number(tempFar.toFixed(1));
 };
 
 // Do not edit below this line
